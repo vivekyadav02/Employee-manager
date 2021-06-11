35dcd4359f5c25fa694401af4eb03abf9f60ccd7
vivekyadav02 <vivek.yadav02@nagarro.com>
2021-06-11 16:27:00 +0530

    init commit

---
+++ src/main/java/com/nagarro/advancedjava/springboot/web/HrManagerLoginApplication.java
@@ -0,0 +1,13 @@
+package com.nagarro.advancedjava.springboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrManagerLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrManagerLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nagarro/advancedjava/springboot/web/HrManagerLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.advancedjava.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrManagerLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
